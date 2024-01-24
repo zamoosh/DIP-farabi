fbd4e63353ec2d5da04d9737294b96425c70f508
Mohammad Rahimi <moyi.pary@gmail.com>
2024-01-24 18:49:07 +0330

    geo_mean lowpass completed

--- geometric_mean.py
+++ geometric_mean.py
@@ -1,23 +1,46 @@
+import numpy
 import cv2
-import numpy as np
-import matplotlib.pyplot as plt
+from library.colorful_logging import color_print
 
-img = cv2.imread('./source_images/cameraman.png', cv2.IMREAD_GRAYSCALE).astype(float)
-rows, cols = img.shape[:2]
-ksize = 5
+PATH = './source_images/cameraman.png'
 
-padsize = int((ksize - 1) / 2)
-pad_img = cv2.copyMakeBorder(img, *[padsize] * 4, cv2.BORDER_DEFAULT)
-geomean1 = np.zeros_like(img)
-for r in range(rows):
-    for c in range(cols):
-        geomean1[r, c] = np.prod(pad_img[r:r + ksize, c:c + ksize]) ** (1 / (ksize ** 2))
-geomean1 = np.uint8(geomean1)
-cv2.imshow('1', geomean1)
-cv2.waitKey()
 
-# geomean2 = np.uint8(np.exp(cv2.boxFilter(np.log(source_images), -1, (ksize, ksize))))
-# plt.imshow(geomean2, cmap='gray')
-# cv2.imshow('2', geomean2)
-# cv2.waitKey()
+def show(geo_mean):
+    geo_mean = numpy.uint8(geo_mean)
+    cv2.imshow('1', geo_mean)
+    cv2.waitKey()
 
+
+def calculate_mean(img, padding, rows, cols, kernel):
+    pad_img = cv2.copyMakeBorder(
+        img, *[padding] * 4, cv2.BORDER_DEFAULT
+    )
+
+    geo_mean = numpy.zeros_like(img)
+
+    for row in range(rows):
+        for col in range(cols):
+            geo_mean[row, col] = numpy.prod(
+                pad_img[row:row + kernel, col:col + kernel]
+            ) ** (1 / (kernel ** 2))
+
+    return geo_mean
+
+
+def main():
+    img = cv2.imread(PATH, cv2.IMREAD_GRAYSCALE).astype(float)
+
+    rows, cols = img.shape[:2]
+    kernel = 5
+    padding = int(
+        (kernel - 1) / 2
+    )
+    geo_mean = calculate_mean(img, padding, rows, cols, kernel)
+    show(geo_mean)
+    return
+
+
+if __name__ == '__main__':
+    color_print('PLEASE ENTER esc TO EXIT', 'bold')
+    main()
+    color_print('ENDED', 'green')
